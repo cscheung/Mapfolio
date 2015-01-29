2cfb02e9fbb40ed29e8559a02e3958d2dc0b8d82
Kazbek <kazbek@umail.ucsb.edu>
2015-01-29 13:28:52 -0800

    changed tracker.js to include experimental distance measurements from old repo

--- app/assets/javascripts/tracker.js
+++ app/assets/javascripts/tracker.js
@@ -1,3 +1,6 @@
+var count, avg_x, avg_y, avg_z, distance_x, distance_y, time0, timeT;
+
+
 function start_tracking()
 {
     
@@ -10,18 +13,25 @@ function start_tracking()
     
     
     gyro.frequency = 100;
-    
+    count = 0;
+    avg_x = 0;
+    avg_y = 0;
+    avg_z = 0;
+    distance_x = 0;
+    distance_y = 0;
+    var d = new Date();
+    time0 = d.getTime();
+
     gyro.startTracking(function(o) 
         {
             document.getElementById("accelerometer_x").innerHTML = 
-            "accelerometer_x: " + Number((o.x).toFixed(1));
+            "accelerometer_x: " + Number(o.x);
             
             document.getElementById("accelerometer_y").innerHTML = 
-            "accelerometer_y: " + Number((o.y).toFixed(1));
+            "accelerometer_y: " + Number(o.y);
             
             document.getElementById("accelerometer_z").innerHTML = 
-            "accelerometer_z: " + Number((o.z).toFixed(1));
-    
+            "accelerometer_z: " + Number(o.z);
              
             document.getElementById("gyroscope_alpha").innerHTML = 
             "gyroscope_alpha: " + Number((o.alpha).toFixed(1));
@@ -31,6 +41,18 @@ function start_tracking()
             
             document.getElementById("gyroscope_gamma").innerHTML = 
             "gyroscope_gamma: " + Number((o.gamma).toFixed(1));
+
+            document.getElementById("avg_x").innerHTML = 
+            "avg_x: " + Number(avg_x);
+            document.getElementById("avg_y").innerHTML = 
+            "avg_y: " + Number(avg_y);
+            document.getElementById("avg_z").innerHTML = 
+            "avg_z: " + Number(avg_z);
+
+            avg_x = ((o.x + avg_x*count) / (count + 1)).toFixed(2);
+            avg_y = ((o.y + avg_y*count) / (count + 1)).toFixed(2);
+            avg_z = ((o.z + avg_z*count) / (count + 1)).toFixed(2);
+            count++;
         }
     );
 }
@@ -38,6 +60,18 @@ function start_tracking()
 function stop_tracking()
 {
     gyro.stopTracking();
+    var d = new Date();
+    timeT = d.getTime();
+    timeT = (timeT - time0) / 1000
+    distance_x = (0.5) * avg_x * timeT * timeT;
+    distance_y = (0.5) * avg_y * timeT * timeT;
+    document.getElementById("distance_x").innerHTML = 
+            "distance_x: " + Number(distance_x);
+    document.getElementById("distance_y").innerHTML = 
+            "distance_y: " + Number(distance_y);
+            document.getElementById("time").innerHTML = 
+            "Time elapsed: " + Number(timeT);
+
 }
 
 function put_values_in_article()
