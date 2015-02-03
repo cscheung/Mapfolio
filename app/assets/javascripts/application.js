dce0b73738ebbdda73f25b07a357296d2fbcc573
thetomcraig <thetomcraig@icloud.com>
2015-02-03 10:56:59 -0800

    Reworked the javascript to be kicked of by clicking a DIV

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,4 @@
 //= require_tree .
 
 //= require tracker
-
+//= require gyro
--- app/assets/javascripts/tracker.js
+++ app/assets/javascripts/tracker.js
@@ -1,32 +1,29 @@
+$( document ).ready(function() 
+{
+    document.getElementById("calibrate").onclick=function(){calibrate()};
+    document.getElementById("start_tracking").onclick=function(){start_tracking()};
+});
+
+var accelerometer_active = {x:0, y:0, z:0};
+var gyroscope_active = {alpha:0, beta:0, gamma:0}; 
+
+var accelerometer_origin = {x:0, y:0, z:0};
+var gyroscope_origin = {alpha:0, beta:0, gamma:0}; 
+
 function start_tracking()
 {
-    
-	var accelerometer_x;  
-	var accelerometer_y;
-	var accelerometer_z;
-
-	var gyro_alpha;  
-	var gyro_beta;
-	var gyro_gamma; 
-    
     gyro.frequency = 100;
     
+    console.log("tracking!!");
     gyro.startTracking(function(o) 
     {
-   		accelerometer_x = Number((o.x).toFixed(1));
-        accelerometer_y = Number((o.y).toFixed(1));
-        accelerometer_z = Number((o.z).toFixed(1));
+   		accelerometer_active.x = Number((o.x).toFixed(1));
+        accelerometer_active.y = Number((o.y).toFixed(1));
+        accelerometer_active.z = Number((o.z).toFixed(1));
    
-        gyro_alpha = Number((o.alpha).toFixed(1));
-        gyro_beta = Number((o.beta).toFixed(1));
-        gyro_gamma = Number((o.gamma).toFixed(1)); 
-        
-        console.log(accelerometer_x);
-        console.log(accelerometer_y);
-        console.log(accelerometer_z);
-        console.log(gyro_alpha);
-        console.log(gyro_alpha);
-        console.log(gyro_alpha);
+        gyroscope_active.alpha = Number((o.alpha).toFixed(1));
+        gyroscope_active.beta = Number((o.beta).toFixed(1));
+        gyroscope_active.gamma = Number((o.gamma).toFixed(1));     
     });
 }
 
@@ -35,6 +32,18 @@ function stop_tracking()
     gyro.stopTracking();
 }
 
+function calibrate()
+{
+    gyroscope_origin.alpha = gyroscope_active.alpha;
+    gyroscope_origin.beta = gyroscope_active.beta;
+    gyroscope_origin.gamma = gyroscope_active.gamma;
+    
+    accelerometer_origin.x = accelerometer_active.x;
+    accelerometer_origin.y = accelerometer_active.y;
+    accelerometer_origin.z = accelerometer_active.z;
+    
+    alert(accelerometer_origin.y);
+}
 function average_measurement()
 {
     
