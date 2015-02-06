28e3871a6037b436279492aa507604d18a089930
Kazbek <kazbek@umail.ucsb.edu>
2015-02-06 14:17:41 -0800

    testing reorienting vectors using gyro

--- app/assets/javascripts/tracker.js
+++ app/assets/javascripts/tracker.js
@@ -22,77 +22,59 @@ function start_tracking()
     false);
     
     
-    gyro.frequency = 10; // 100 Hz sampling frequency
+    gyro.frequency = 100; // 10 Hz sampling frequency
     
     acc_x = 0;
     acc_y = 0;
     acc_z = 0   
 
-    prev_acc_x = 0;
-    prev_acc_y = 0;
-    prev_acc_z = 0;
-
-    pos_x = 0;
-    pos_y = 0;
-
-    velocity_x = 0;
-    velocity_y = 0;
-
     var t = gyro.frequency * 0.001;
 
 
     gyro.startTracking(function(o) 
-        {
-             
-            //document.getElementById("gyroscope_alpha").innerHTML = 
-            //"gyroscope_alpha: " + Number((o.alpha).toFixed(1));
-                        
-            //document.getElementById("gyroscope_beta").innerHTML = 
-            // "gyroscope_beta: " + Number((o.beta).toFixed(1));
+        {             
+            acc_x = o.x;
+            acc_y = o.y;
+            acc_z = o.z;
+
+            var reoriented_values = rotateVector (acc_x, acc_y, acc_y, -1.0*o.alpha, -1.0*o.beta, -1.0*o.gamma); 
+            var r_acc_x = reoriented_values[0]
+            var r_acc_y = reoriented_values[1]
+            var r_acc_z = reoriented_values[2]
+
+            // pos_x += (0.5*interpolated_x*t*t) + velocity_x*t;
+            // pos_y += (0.5*interpolated_y*t*t) + velocity_y*t;
+
+            // velocity_x += interpolated_x * t;
+            // velocity_y += interpolated_y * t;
+
             
-            // document.getElementById("gyroscope_gamma").innerHTML = 
-            // "gyroscope_gamma: " + Number((o.gamma).toFixed(1));
-
-            acc_x = Math.round((o.x - compensate_acc_x)*100)/100;
-            acc_y = Math.round((o.y - compensate_acc_y)*100)/100;
-            acc_z = Math.round((o.z - compensate_acc_z)*100)/100;
-
-            interpolated_x = (prev_acc_x + acc_x) / 2;
-            interpolated_y = (prev_acc_y + acc_y) / 2;
-            interpolated_z = (prev_acc_z + acc_z) / 2;
-
-            var reoriented_values = rotateVector (interpolated_x, interpolated_y, interpolated_z, -1.0*o.alpha, -1.0*o.beta, -1.0*o.gamma); 
-            var r_acc_x = interpolated_x - reoriented_values[0]
-            var r_acc_y = interpolated_y - reoriented_values[1]
-            var r_acc_z = interpolated_z - reoriented_values[2]
-
-            pos_x += (0.5*interpolated_x*t*t) + velocity_x*t;
-            pos_y += (0.5*interpolated_y*t*t) + velocity_y*t;
-
-            velocity_x += interpolated_x * t;
-            velocity_y += interpolated_y * t;
-
-            prev_acc_x = acc_x;
-            prev_acc_y = acc_y;
-
+            document.getElementById("gyroscope_alpha").innerHTML = 
+            "gyroscope_alpha: " + Number((o.alpha).toFixed(1));            
+            document.getElementById("gyroscope_beta").innerHTML = 
+            "gyroscope_beta: " + Number((o.beta).toFixed(1));
+            document.getElementById("gyroscope_gamma").innerHTML = 
+            "gyroscope_gamma: " + Number((o.gamma).toFixed(1));
             document.getElementById("accelerometer_x").innerHTML = 
             "accelerometer_x: " + Number(r_acc_x);
             document.getElementById("accelerometer_y").innerHTML = 
             "accelerometer_y: " + Number(r_acc_y);    
             document.getElementById("accelerometer_z").innerHTML = 
             "accelerometer_z: " + Number(r_acc_z);
-            document.getElementById("velocity_x").innerHTML = 
-            "velocity_x: " + Number(velocity_x);
-            document.getElementById("velocity_y").innerHTML = 
-            "velocity_y: " + Number(velocity_y);
-            document.getElementById("pos_x").innerHTML = 
-            "pos_x: " + Number(pos_x);
-            document.getElementById("pos_y").innerHTML = 
-            "pos_y: " + Number(pos_y);
+            // document.getElementById("velocity_x").innerHTML = 
+            // "velocity_x: " + Number(velocity_x);
+            // document.getElementById("velocity_y").innerHTML = 
+            // "velocity_y: " + Number(velocity_y);
+            // document.getElementById("pos_x").innerHTML = 
+            // "pos_x: " + Number(pos_x);
+            // document.getElementById("pos_y").innerHTML = 
+            // "pos_y: " + Number(pos_y);
             document.getElementById("compensate_acc_x").innerHTML = 
             "compensate_acc_x: " + Number(compensate_acc_x);
-            document.getElementById("compensate_acc_y").innerHTML = 
+            document.getElementById("compensate_acc_y").innerHTML 
             "compensate_acc_y: " + Number(compensate_acc_y);
+            document.getElementById("compensate_acc_z").innerHTML = 
+            "compensate_acc_z: " + Number(compensate_acc_z); 
         }
     );
 }
@@ -109,10 +91,7 @@ function calibrate()
         {
             compensate_acc_x = o.x; 
             compensate_acc_y = o.y;
-            document.getElementById("compensate_acc_x").innerHTML = 
-            "compensate_acc_x: " + Number(compensate_acc_x);
-            document.getElementById("compensate_acc_y").innerHTML = 
-            "compensate_acc_y: " + Number(compensate_acc_y);    
+            compensate_acc_z = o.z;
         }
     );
 }
