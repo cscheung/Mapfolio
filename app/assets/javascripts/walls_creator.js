f052dea8d4376c725c85db0df8d0ae7024d61cf5
thetomcraig <thetomcraig@icloud.com>
2015-02-15 18:26:40 -0800

    Wokring on the walls setup, able to read all points from the points db, should do this in the floorplan creator, but in the walls creator for now

---
+++ app/assets/javascripts/walls_creator.js
@@ -0,0 +1,12 @@
+var points_array;
+
+function load_points()
+{
+    console.log(players = $('.points_class').data('points'));
+}
+
+function show_points()
+{
+    
+}
+
--- app/controllers/walls_controller.rb
+++ app/controllers/walls_controller.rb
@@ -15,7 +15,7 @@ class WallsController < ApplicationController
   # GET /walls/new
   def new
     @wall = Wall.new
-    @points = Point.where(:wall_id => 0)
+    @points = Point.all
   end
 
   # GET /walls/1/edit
