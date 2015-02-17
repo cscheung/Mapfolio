c090c645efb87eacd42625fb9351a4dbb3e79e5d
Kazbek <kazbek@umail.ucsb.edu>
2015-02-16 22:29:56 -0800

    gets the data from fulltilt

--- app/assets/javascripts/tracker.js
+++ app/assets/javascripts/tracker.js
@@ -1,24 +1,61 @@
-var acc_x, acc_y, acc_z, pos_x, pos_y, velocity_x, velocity_y;
-
-var prev_acc_x, prev_acc_y, prev_z;
-
-var interpolated_x, interpolated_y, interpolated_z;
-
-var compensate_acc_x = 0; 
-var compensate_acc_y = 0;
-var compensate_acc_z = 0;
-
-//var time_start, time_finish;
-
-
-function start_tracking()
-{
-    
-}
-
 function stop_tracking()
 {
+	var screenAccX = document.getElementById("accelerometer_x");
+	var screenAccY = document.getElementById("accelerometer_y");
+	var screenAccZ = document.getElementById("accelerometer_z");
 
+	screenAccX.innerHTML = "Stopped";
+	screenAccY.innerHTML = "Stopped";
+	screenAccZ.innerHTML = "Stopped";
 }
+
+function printDataValue(input) {
+	if( input === undefined )
+		return "undefined";
+	if( input === null )
+		return "null";
+	if( input === true )
+		return "true";
+	if( input === false )
+		return "false";
+	if( Object.prototype.toString.call(input) === "[object Number]" )
+		return Math.round((input + 0.00001) * 100) / 100; // return to 2 decimal places
+
+	return (input + ""); // force stringify
+}
+
+function start_tracking() {
+
+	var screenAccX = document.getElementById("accelerometer_x");
+	var screenAccY = document.getElementById("accelerometer_y");
+	var screenAccZ = document.getElementById("accelerometer_z");
+
+	// Start FULLTILT DeviceMotion listeners and register our callback
+	var deviceMotion = FULLTILT.getDeviceMotion();
+	deviceMotion.then(function(motionData) {
+
+		// Checking availability
+	//	accXAvl.textContent = (motionData.isAvailable(motionData.ACCELERATION_X)) ? '+' : '-' ;
+	//	accYAvl.textContent = (motionData.isAvailable(motionData.ACCELERATION_Y)) ? '+' : '-' ;
+	//	accZAvl.textContent = (motionData.isAvailable(motionData.ACCELERATION_Z)) ? '+' : '-' ;
+
+	//	rotRateAlphaAvl.textContent = (motionData.isAvailable(motionData.ROTATION_RATE_ALPHA)) ? '+' : '-' ;
+	//	rotRateBetaAvl.textContent = (motionData.isAvailable(motionData.ROTATION_RATE_BETA)) ? '+' : '-' ;
+	//	rotRateGammaAvl.textContent = (motionData.isAvailable(motionData.ROTATION_RATE_GAMMA)) ? '+' : '-' ;
+
+		motionData.listen(function() {
+
+			// Display calculated screen-adjusted devicemotion
+
+			var screenAcc = motionData.getScreenAdjustedAcceleration() || {};
+
+			screenAccX.innerHTML = "acc X = " + printDataValue(screenAcc.x);
+			screenAccY.innerHTML = "acc Y = " + printDataValue(screenAcc.y);
+			screenAccZ.innerHTML = "acc Z = " + printDataValue(screenAcc.z);
+
+		});
+
+	});
+
 }
 
