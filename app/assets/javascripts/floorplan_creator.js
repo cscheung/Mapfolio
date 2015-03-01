5711d4b4d5ae45ba7731c65800936605b8fb86f8
thetomcraig <thetomcraig@icloud.com>
2015-03-01 15:37:48 -0800

    Added vertecies to the walls so they can be mutated, added camera code

--- app/assets/javascripts/floorplan_creator.js
+++ app/assets/javascripts/floorplan_creator.js
@@ -1,13 +1,14 @@
 var HEIGHT = 500;
 var WIDTH = 500;
 var SCALING_FACTOR = 1;
-var X_TRANSLATION = 100;
-var Y_TRANSLATION = 100;	
-    
+var X_TRANSLATION = 75;
+var Y_TRANSLATION = 75;	
+var VERTEX_RADIUS = 10;
+
 points_array = [];
 intersections_array = [];
+walls_array = [];
 var canvas;
-
 
 $(document).ready(function(){
     
@@ -18,31 +19,52 @@ $(document).ready(function(){
 	});
 	
 	
-	canvas.on('mouse:down', function(e) {
-    if(e.target)
+    canvas.on('mouse:down', function(e) 
     {
-        //Call a js function
-        console.log("toggle!");   
-        canvas.renderAll();
-    }
-  });
+        if(e.target)
+        {
+            //Call a js function
+            display_photo(e.target);
+            canvas.renderAll();
+        }
+    });
+  
+    canvas.on('object:moving', function(e) 
+    {
+        if(e.target.name == 'vertex')
+        {
+            var p = e.target;
+            p.wall1.set({'x2' : p.left + VERTEX_RADIUS, 'y2' : p.top + VERTEX_RADIUS});
+            p.wall2.set({'x1' : p.left + VERTEX_RADIUS, 'y1' : p.top + VERTEX_RADIUS});    
+            canvas.renderAll();
+        }
+    });
 
 	  
 });
 
-
-
 /*camera code*/
-function show_camera_icon()
+function display_photo(canvas_object)
+{
+    if (canvas_object.name == "camera")
+    {
+        console.log("camera clicked");
+    }
+}
+
+function create_camera_icon()
 {
     fabric.Image.fromURL('camera.png', function(oImg) 
     {
+        oImg.name = "camera";
+        oImg.scale(0.5);
+        oImg.set('selectable', false);
         canvas.add(oImg);
     });
 }
 function previewFile() {
 
-show_camera_icon();
+create_camera_icon();
 
 var preview = document.querySelector('img');
 var file = document.querySelector('input[type=file]').files[0];
@@ -60,6 +82,12 @@ preview.src = "";
 }
 /*camera code*/
 
+function draw_floorplan()
+{
+    load_points();
+    draw_walls();
+}
+
 function load_points()
 {
     points = $('.points_class').data('points');
@@ -74,7 +102,48 @@ function load_points()
             
         points_array.push(new_point);
     }
+}
 
+function draw_walls()
+{
+    //Find interctions
+    for (i = 0; i < points_array.length; i++) 
+    {
+        var p0 = points_array[i];
+        var p1 = points_array[(i+1)%points_array.length];
+        var p2 = find_intersection(p0, p1);
+
+        intersections_array.push(p2);
+    }
+    
+    //Find walls
+    for (i = 0; i < intersections_array.length; i++) 
+    {
+        var a = intersections_array[i];
+        var b = intersections_array[(i+1)%intersections_array.length];
+        
+        var translated_points = translate_points([a.x, a.y, b.x, b.y]);
+        walls_array.push(make_wall(translated_points));
+    }
+    
+    //Put on walls    
+    for (i = 0; i < walls_array.length; i++)
+    {
+        canvas.add(walls_array[i]);
+    }
+    
+    //Make intersections
+    //Put them on canvas
+    for (i = 0; i < walls_array.length; i++)
+    {
+        var vertex = make_vertex(
+        walls_array[i].x2, 
+        walls_array[i].y2, 
+        walls_array[i], 
+        walls_array[(i+1)%points_array.length]);
+        
+        canvas.add(vertex);
+    } 
 }
 
 function find_intersection(p0, p1)
@@ -112,45 +181,44 @@ function find_intersection(p0, p1)
 
     return {x: x_intersection, y: y_intersection};
 }
-	
-function draw_walls()
-{
-    for (i = 0; i < points_array.length; i++) 
-    {
-        var p0 = points_array[i];
-        var p1 = points_array[(i+1)%points_array.length];
-        var p2 = find_intersection(p0, p1);
 
-        intersections_array.push(p2);
-    }
-    
-    for (i = 0; i < intersections_array.length; i++) 
+function make_wall(coords) 
+{
+    return new fabric.Line(coords, 
     {
-        var a = intersections_array[i];
-        var b = intersections_array[(i+1)%intersections_array.length];
-        var line = draw_line_from_points(a.x, a.y, b.x, b.y);
-        canvas.add(line);
-    }
-    
-    
-    
+      fill: 'black',
+      stroke: 'black',
+      strokeWidth: 5,
+      selectable: false
+    });
 }
 
-function draw_line_from_points(x1, y1, x2, y2) 
-{    
-	var line = new fabric.Line(
-	[
-	x1*SCALING_FACTOR + X_TRANSLATION, 
-	(WIDTH - y1)*SCALING_FACTOR - Y_TRANSLATION, 
-	x2*SCALING_FACTOR + X_TRANSLATION, 
-	(WIDTH - y2)*SCALING_FACTOR - Y_TRANSLATION
-	], 
-	{
-		strokeWidth: '1',
-		stroke: 'black', 
+function make_vertex(left, top, wall1, wall2) 
+{
+    var c = new fabric.Circle(
+    {
+        left: left - VERTEX_RADIUS,
+        top: top - VERTEX_RADIUS,
+        strokeWidth: 2,
+        radius: VERTEX_RADIUS,
+        fill: '#fff',
+        stroke: '#666',
+        name: 'vertex'
     });
     
-	return line;
+    c.wall1 = wall1;
+    c.wall2 = wall2;
+    c.hasControls = c.hasBorders = false;
+    return c;
 }
 
+function translate_points(points)
+{
+    points[0] = points[0]*SCALING_FACTOR + X_TRANSLATION;
+    points[1] = (WIDTH - points[1])*SCALING_FACTOR - Y_TRANSLATION;
+    points[2] = points[2]*SCALING_FACTOR + X_TRANSLATION;
+    points[3] = (WIDTH - points[3])*SCALING_FACTOR - Y_TRANSLATION;
+    
+    return points;
+}
 
